4694058e1adf4fe20aebb7e381f187620dec74be
VD web developer <jason.5001001@gmail.com>
2017-01-12 07:41:21 -0600

    fix unicode issue in notification

--- posts/views.py
+++ posts/views.py
@@ -1650,7 +1650,8 @@ Rate comment method.
                         "post_id": post.id,
                         "avatar":  UserProfile.objects.get(user=token.user).avatar.url
                     }
-                    message = "{} {}d: {}".format(token.user.username, action.lower(), comment.text.decode('ascii', 'ignore'))
+                    msg = comment.text.decode('ascii', 'ignore')
+                    message = "{} {}d: {}".format(token.user.username, action.lower(), msg)
                     user_notification = UserNotification.objects.get(user=comment.author)
                     send_notification(custom, message, user_notification)
 
